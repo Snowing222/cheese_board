46481971d2500765066fcdacaab76b10e2e89d03
elsa chen <snowing222@gmail.com>
2020-06-15 14:12:40 +0000

    first commit

---
+++ lib/cheese_board.rb
@@ -0,0 +1,6 @@
+require "cheese_board/version"
+
+module CheeseBoard
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cheese_board/version.rb
@@ -0,0 +1,3 @@
+module CheeseBoard
+  VERSION = "0.1.0"
+end
