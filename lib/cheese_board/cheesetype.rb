6a001802da2c8aaff07f91d04f47605547719ef6
elsa chen <snowing222@gmail.com>
2020-06-16 19:19:20 +0000

    start scraping cheeses

--- lib/cheese_board/cheesetype.rb
+++ lib/cheese_board/cheesetype.rb
@@ -1,15 +1,21 @@
 class CheeseBoard::CheeseType
   @@all=[]
-  attr_accessor :name, :description, :char
+  attr_accessor :name, :description, :cheese
 
   def initialize(name)
     @name=name
+    @cheeses=[]
     save
   end
 
   def self.all
     CheeseBoard::Scraper.scrape_cheese_types if @@all.empty?
     @@all
+  end
+
+  def cheeses
+    CheeseBoard::Scraper.scrape_cheeses if @cheeses.empty?
+
   end
 
   def save
--- lib/cheese_board/cli.rb
+++ lib/cheese_board/cli.rb
@@ -34,6 +34,7 @@ class CheeseBoard::CLI
 
   def show_cheese_type_for(chosen_cheese_type)
     cheese_type=@cheese_types[chosen_cheese_type-1]
+    cheeses = cheese_type.cheeses
     puts "Here are the information for #{cheese_type.name}"
 
     # CheeseBoard::Cheese.all.each.with_index(1) do |cheese|
