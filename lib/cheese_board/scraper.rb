1d01403503730979c57f849030b9068da5f9eea2
elsa chen <snowing222@gmail.com>
2020-06-16 21:36:21 +0000

    basic logic works

--- lib/cheese_board/scraper.rb
+++ lib/cheese_board/scraper.rb
@@ -1,22 +1,23 @@
 class CheeseBoard::Scraper
-
+  BASEPATH="https://www.castellocheese.com/en-us/cheese-types/"
   def self.scrape_cheese_types
-    doc = Nokogiri::HTML(open("https://www.castellocheese.com/en-us/cheese-types/"))
+    doc = Nokogiri::HTML(open(BASEPATH))
     cheese_types=doc.css("div.generic-overview-grid--no-scroll a.generic-card__link")
     cheese_types.map do |cheese_type|
       name=cheese_type.text
       CheeseBoard::CheeseType.new(name)
     end
-
-    #return array of cheese type
-  end
-
-  def cheese_type_to_url(cheese_type)
-    #takes in blue mold cheese to blue-mold-cheese/
   end
 
   def self.scrape_cheeses(cheese_type)
-    CheeseBoard::Cheese.new("abc",cheese_type)
-    CheeseBoard::Cheese.new("def",cheese_type)
+    url=BASEPATH + cheese_type.name.gsub(" ","-") + "/"
+    doc = Nokogiri::HTML(open(url))
+    heads=doc.css("div.cheese-category__inner.row-container h4")
+    heads.shift
+    heads.each.with_index do |h|
+      name=h.text
+      CheeseBoard::Cheese.new(name,cheese_type)
+    end
   end
+
 end
