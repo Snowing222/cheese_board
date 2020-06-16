63bf79153c601f3e491b2f8c5d4d6cc7752732c5
elsa chen <snowing222@gmail.com>
2020-06-16 13:07:35 +0000

    CLI-1

--- lib/cheese_board/cli.rb
+++ lib/cheese_board/cli.rb
@@ -1,10 +1,22 @@
 class CheeseBoard::CLI
   def call
-    list_of_cheese_type
+    puts "Welcome to Cheese_board! Are you ready to explore the CHEEZZZYY-WORLD?"
+    puts ""
+    get_list_of_cheese_type
+    get_user_cheese_type
+    #get_cheese_for(cheese_type)
+    #list cheeses
   end
 
-  def list_of_cheese_type
-    puts "cheese"
+  def get_list_of_cheese_type
+    #to be scraped instead
+    @cheese_types=["hard cheese", "soft cheese" ,"fresh cheese", "goat cheese"]
+  end
 
+  def get_user_cheese_type
+    #list cheese type
+    @cheese_types.each.with_index(1) do |cheese_type, index|
+      puts "#{index}. #{cheese_type}"
+    end
   end
 end
