ff136d20b054d59127b2bf87cf70fc45bb0fe1aa
elsa chen <snowing222@gmail.com>
2020-06-17 02:41:34 +0000

    add get_user_cheese

--- lib/cheese_board/cli.rb
+++ lib/cheese_board/cli.rb
@@ -4,6 +4,7 @@ class CheeseBoard::CLI
     get_cheese_types
     list_cheese_types
     get_user_cheese_type
+    get_user_cheese
     #get_cheese_for(cheese_type)
     #list cheeses
 
@@ -27,7 +28,6 @@ class CheeseBoard::CLI
     else
       say_good_bye
     end
-
   end
 
   def valid_input?(input,array)
@@ -39,12 +39,30 @@ class CheeseBoard::CLI
   end
 
   def show_cheese_type_for(chosen_cheese_type)
-    cheese_type=@cheese_types[chosen_cheese_type-1]
-    cheese_type.get_cheeses
-    puts "Here are the information for #{cheese_type.name}"
-    cheese_type.cheeses.each.with_index(1) do |cheese,index|
+    @cheese_type=@cheese_types[chosen_cheese_type-1]
+    @cheese_type.get_cheeses
+    puts "Here are the information for #{@cheese_type.name}"
+    puts "Enter a number for the cheese you are interested in, 'back; to go back, or exit"
+    @cheese_type.cheeses.each.with_index(1) do |cheese,index|
       puts "#{index}. #{cheese.name}"
     end
+  end
+
+  def get_user_cheese
+    input = gets.strip
+    case input
+    when "exit"
+      say_good_bye
+    when "back"
+      list_cheese_types
+      get_user_cheese_type
+    else
+      show_cheese_for(input.to_i) if valid_input?(input, @cheese_type.cheeses)
+    end
+  end
+
+  def show_cheese_for(chosen_cheese)
+    puts "here is the info"
   end
 
 end
