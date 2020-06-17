f4264dd6d0f4123195081cd6df80bccd7312fb24
elsa chen <snowing222@gmail.com>
2020-06-17 12:28:52 +0000

    finish

--- lib/cheese_board/cli.rb
+++ lib/cheese_board/cli.rb
@@ -11,7 +11,7 @@ class CheeseBoard::CLI
 
   def list_cheese_types
     get_cheese_types
-    puts "\nWhat type of cheese are you interested in. Please enter number of the cheese, or exit"
+    puts "\nWhat type of cheese are you interested in. Please enter number of the cheese, or 'exit'"
     @cheese_types.each.with_index(1) do |cheese_type, index|
       puts "#{index}. #{cheese_type.name}"
     end
@@ -22,7 +22,10 @@ class CheeseBoard::CLI
   end
 
   def get_user_cheese_type
+    puts ""
     input = gets.strip
+    puts ""
+    sleep 1
     if input != "exit"
       show_cheese_type_for(input.to_i) if valid_input?(input, @cheese_types)
     else
@@ -39,14 +42,17 @@ class CheeseBoard::CLI
     @cheese_type.get_cheeses
     puts "Here are the information for #{@cheese_type.name}"
     puts "\nIt's #{@cheese_type.char}"
-    puts "\nEnter a number for the cheese you are interested in, 'back; to go back, or exit"
+    puts "\nEnter a number for the cheese you are interested in, 'back' to discover more,or 'exit'"
     @cheese_type.cheeses.each.with_index(1) do |cheese,index|
       puts "#{index}. #{cheese.name}"
     end
   end
 
   def get_user_cheese
+    puts ""
     input = gets.strip
+    puts ""
+    sleep 1
     case input
     when "exit"
       say_good_bye
@@ -62,12 +68,15 @@ class CheeseBoard::CLI
   def show_cheese_for(chosen_cheese)
     cheese = @cheese_type.cheeses[chosen_cheese-1]
     CheeseBoard::Scraper.scrape_cheese_descriptions
-    puts "\n#{cheese.cheese_description}"
+    puts "#{cheese.cheese_description}"
     puts "\n#{cheese.pair_wine}"
+    puts "\nEnter 'back' to discover more cheeses, or 'exit'"
+    get_user_cheese
  end
 
  def say_good_bye
    puts "See you next time!"
+   exit!
  end
 
 end
