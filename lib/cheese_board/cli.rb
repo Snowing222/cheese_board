0b1087771bf442f7bcf00ce405208cd57ddfb2e8
elsa chen <snowing222@gmail.com>
2020-06-17 20:36:38 +0000

    edit cli flow

--- lib/cheese_board/cli.rb
+++ lib/cheese_board/cli.rb
@@ -23,10 +23,13 @@ class CheeseBoard::CLI
     input = gets.strip
     puts ""
     sleep 1
-    if input != "exit"
-      show_cheese_type_for(input.to_i) if valid_input?(input, @cheese_types)
-    else
+    if input=="exit"
       say_good_bye
+    elsif valid_input?(input, @cheese_types)
+      show_cheese_type_for(input.to_i)
+    else
+      puts "I dont understand, please try again"
+      get_user_cheese_type
     end
   end
 
@@ -38,7 +41,7 @@ class CheeseBoard::CLI
     @cheese_type=@cheese_types[chosen_cheese_type-1]
     @cheese_type.get_cheeses
     puts "Here are the information for #{@cheese_type.name}"
-    puts "\nIt's #{@cheese_type.char}"
+    puts "\n#{@cheese_type.char}".colorize(:blue)
     puts "\nEnter a number for the cheese you are interested in, #{ColorizedString["back"].colorize(:red)} to discover more, or #{ColorizedString["exit"].colorize(:red)}"
     @cheese_type.cheeses.each.with_index(1) do |cheese,index|
       puts "#{index}. #{cheese.name}"
@@ -50,25 +53,32 @@ class CheeseBoard::CLI
     input = gets.strip
     puts ""
     sleep 1
-    case input
-    when "exit"
+
+    if input=="exit"
       say_good_bye
-    when "back"
+    elsif input=="back"
       list_cheese_types
       get_user_cheese_type
       get_user_cheese
+    elsif valid_input?(input, @cheese_type.cheeses)
+      show_cheese_for(input.to_i)
     else
-      show_cheese_for(input.to_i) if valid_input?(input, @cheese_type.cheeses)
+      puts "I dont understand, please try again"
+      get_user_cheese_type
     end
   end
 
   def show_cheese_for(chosen_cheese)
     cheese = @cheese_type.cheeses[chosen_cheese-1]
     CheeseBoard::Scraper.scrape_cheese_descriptions
+    puts "CHARACTERISTICS".colorize(:blue)
     puts "#{cheese.cheese_description}"
-    puts "\n#{cheese.pair_wine}"
+    puts ""
+    puts "BEST MATE".colorize(:blue)
+    puts "#{cheese.pair_wine}"
     puts "\nEnter #{ColorizedString["back"].colorize(:red)} to discover more cheeses, or #{ColorizedString["exit"].colorize(:red)}"
     get_user_cheese
+
  end
 
  def say_good_bye
